d92fa7b1704f378cef3ec790f450634751f3cf1c
computermouth <computermouth@crunchbangplusplus.org>
2016-03-10 10:50:18 -0800

    Added/moved set -x for debugging

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
 set -x
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 function setup {
 	rm -rf rootfs*
@@ -33,6 +33,8 @@ EOF
 #  sudo mount -t devpts   chpts   rootfs/dev/pts
 
         sudo chroot rootfs /bin/bash <<EOF
+set -x
+
 echo -e "chip\nchip\n" | passwd
 echo "chip" >/etc/hostname
 echo -e "127.0.0.1\tchip" >/tmp/hosts.tmp
